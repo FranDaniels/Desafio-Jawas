0426c82665be57063a89c9ad309641f78994bbdb
Marina Laguna Valdepeñas <marinalaguna2004@gmail.com>
2023-11-19 16:22:31 +0100

    creada las migrations

---
+++ Backend/database/migrations/2023_11_19_150114_create_lote_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lote', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_empresa')->constrained('empresa');
+            $table->string('ubicacion');
+            $table->string('estado');
+            $table->dateTime('fecha_entrega');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lote');
+    }
+};

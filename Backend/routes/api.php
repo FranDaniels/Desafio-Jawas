dc13d266f320dfea3b8e2a891b6c2a094860be78
Francisco <coheteomega@gmail.com>
2023-11-22 10:25:46 +0100

    Creación de las rutas

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\controllerRegistro;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('registro',[controllerRegistro::class,'crearUsuario']);
+Route::post('crearRol',[controllerRegistro::class,'crearRol']);
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });

b502c80d356fe25e7e4def11998908c9eb555158
Francisco <coheteomega@gmail.com>
2023-11-23 10:06:01 +0100

    Modificaciones a la llamada asincrona

--- Frontend/registro/registro.js
+++ Frontend/registro/registro.js
@@ -12,15 +12,43 @@ formulario.addEventListener("input",function(){
     botonRegistro.removeAttribute("disabled",true);
 })
 
-botonRegistro.addEventListener("click",function(){
+botonRegistro.addEventListener("click", async function(){
         if (comprobarValidaciones(nombre,apellido,correo,pass,pass2)) {
+          var datos=cargarDatos();
+          console.log('hola'+datos)
+          await crearUsuario(datos);
         }
 })
 
-async function crearUsuario(){     
+function cargarDatos(){
+  var datos={
+    nombre:nombre.value,
+    apellido:apellido.value,
+    correo:correo.value,
+    password:pass.value
+  }
+
+  console.log(datos)
+  return datos;
+}
+
+async function crearUsuario(datos){   
+  console.log(datos)
+  let body = JSON.stringify(
+    {
+        nombre: datos[0],
+        apellido: datos[1],
+        correo: datos[2],
+        password: datos[3],
+    }
+);  
+console.log(body)
        let response = await fetch("http://127.0.0.1:8000/api/registro", { 
          method: "POST",
-         headers: headersList
+         headers: {
+          "Content-Type": "application/json"
+          },
+         body:body
        });
        
        let data = await response.text();

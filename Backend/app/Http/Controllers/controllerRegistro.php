dc0ecc91c198db115c07c48483ca7dce54729640
Francisco <coheteomega@gmail.com>
2023-11-23 10:05:30 +0100

    Pequeña modificacion

--- Backend/app/Http/Controllers/controllerRegistro.php
+++ Backend/app/Http/Controllers/controllerRegistro.php
@@ -15,6 +15,7 @@ class controllerRegistro extends Controller
         $usuario=new User;
 
         $usuario->nombre=$request->get('nombre');
+        $usuario->apellido=$request->get('apellido');
         $usuario->correo=$request->get('correo');
         $usuario->password=$request->get('password');
         $usuario->id_rol=2;
@@ -24,7 +25,7 @@ class controllerRegistro extends Controller
         try {
             $usuario->save();
             $id = $usuario->id;
-            $rolU = new rol_usuario();
+            $rolU = new rol_usuario;
             $rolU->id_rol = 2;
             $rolU->id_usuario=$id;
             $rolU->save();

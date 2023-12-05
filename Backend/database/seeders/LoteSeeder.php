8b0bbd39a44d2eeb6903d0843eaed5e4dc028497
Francisco <coheteomega@gmail.com>
2023-12-05 10:45:07 +0100

    Mas lotes para el seeder

--- Backend/database/seeders/LoteSeeder.php
+++ Backend/database/seeders/LoteSeeder.php
@@ -43,13 +43,94 @@ class LoteSeeder extends Seeder
         ]);
 
         \App\Models\Lote::create([
-            'descripcion' => 'Lote 3',
+            'descripcion' => 'Lote 4',
             'ubicacion' => 'Paseo San Gregorio',
             'estado' => 'En camino',
             'fecha_entrega' => '2023-12-01 12:01:27',
             'disponible' => '0',
             'clasificado'=>'0',
             'id_usuario' => '2'
+        ]);
+        \App\Models\Lote::create([
+            'descripcion' => 'Lote 5',
+            'ubicacion' => 'Paseo San Gregorio',
+            'estado' => 'En camino',
+            'fecha_entrega' => '2023-12-01 12:01:27',
+            'disponible' => '0',
+            'clasificado'=>'0',
+            'id_usuario' => '2'
+        ]);
+        \App\Models\Lote::create([
+            'descripcion' => 'Lote 6',
+            'ubicacion' => 'Paseo San Gregorio',
+            'estado' => 'En camino',
+            'fecha_entrega' => '2023-12-01 12:01:27',
+            'disponible' => '0',
+            'clasificado'=>'0',
+            'id_usuario' => '2'
+        ]);
+        \App\Models\Lote::create([
+            'descripcion' => 'Lote 7',
+            'ubicacion' => 'Paseo San Gregorio',
+            'estado' => 'En camino',
+            'fecha_entrega' => '2023-12-01 12:01:27',
+            'disponible' => '0',
+            'clasificado'=>'0',
+            'id_usuario' => '2'
+        ]);
+        \App\Models\Lote::create([
+            'descripcion' => 'Lote 8',
+            'ubicacion' => 'Paseo San Gregorio',
+            'estado' => 'En camino',
+            'fecha_entrega' => '2023-12-01 12:01:27',
+            'disponible' => '0',
+            'clasificado'=>'0',
+            'id_usuario' => '2'
+        ]);
+        \App\Models\Lote::create([
+            'descripcion' => 'Lote 9',
+            'ubicacion' => 'Paseo San Gregorio',
+            'estado' => 'En camino',
+            'fecha_entrega' => '2023-12-01 12:01:27',
+            'disponible' => '0',
+            'clasificado'=>'0',
+            'id_usuario' => '2'
+        ]);
+        \App\Models\Lote::create([
+            'descripcion' => 'Lote 10',
+            'ubicacion' => 'Paseo San Gregorio',
+            'estado' => 'En camino',
+            'fecha_entrega' => '2023-12-01 12:01:27',
+            'disponible' => '0',
+            'clasificado'=>'0',
+            'id_usuario' => '3'
+        ]);
+        \App\Models\Lote::create([
+            'descripcion' => 'Lote 11',
+            'ubicacion' => 'Paseo San Gregorio',
+            'estado' => 'En camino',
+            'fecha_entrega' => '2023-12-01 12:01:27',
+            'disponible' => '0',
+            'clasificado'=>'0',
+            'id_usuario' => '3'
+        ]);
+        \App\Models\Lote::create([
+            'descripcion' => 'Lote 12',
+            'ubicacion' => 'Paseo San Gregorio',
+            'estado' => 'En camino',
+            'fecha_entrega' => '2023-12-01 12:01:27',
+            'disponible' => '0',
+            'clasificado'=>'0',
+            'id_usuario' => '3'
+        ]);
+        \App\Models\Lote::create([
+            'descripcion' => 'Lote 13',
+            'ubicacion' => 'Paseo San Gregorio',
+            'estado' => 'En camino',
+            'fecha_entrega' => '2023-12-01 12:01:27',
+            'disponible' => '0',
+            'clasificado'=>'0',
+            'id_usuario' => '3'
         ]);
     }
 }

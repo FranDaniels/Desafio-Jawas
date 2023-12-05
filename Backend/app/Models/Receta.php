a438b6da4afc1290c30ad150c8ebf27b055257b1
Marina Laguna <marinalaguna2004@gmail.com>
2023-12-05 12:57:37 +0100

    modelo de la receta creado

---
+++ Backend/app/Models/Receta.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Receta extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'nombre',
+        'descripción'
+    ];
+
+    protected $hidden = [
+        'id'
+    ];
+}

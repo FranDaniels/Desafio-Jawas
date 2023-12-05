d22f92a0ad15b6d7fdf7365e7c825cb25e078b11
Marina Laguna <marinalaguna2004@gmail.com>
2023-12-05 22:35:11 +0100

    seeder de la tabla inventario

--- Backend/database/seeders/InventarioSeeder.php
+++ Backend/database/seeders/InventarioSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+/**
+ * @author Marina Laguna
+ */
 class InventarioSeeder extends Seeder
 {
     /**

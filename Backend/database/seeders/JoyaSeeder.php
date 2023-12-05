fefbbf90d244f2c55ec7f1a4d5816ba25114d849
Marina Laguna <marinalaguna2004@gmail.com>
2023-12-05 22:34:26 +0100

    creacion del modelo y del seeder de joya

--- Backend/database/seeders/JoyaSeeder.php
+++ Backend/database/seeders/JoyaSeeder.php
@@ -1,45 +1,44 @@
 <?php
 
-use Illuminate\Database\Seeder;
-use App\Models\Joya;
-use App\Models\Receta;
-use App\Models\Componente;
+namespace Database\Seeders;
 
+use Illuminate\Database\Seeder;
+
+/**
+ * @author Marina Laguna
+ */
 class JoyaSeeder extends Seeder
 {
     public function run()
     {
-        $joya = Joya::create([
+        \App\Models\Joya::create([
             'nombre' => 'Pulsera de placa base',
-            'descripcion' => 'Una elegante pulsera con detalles de placa base',
+            'descripcion' => 'Una elegante pulsera de cuero con detalles de placa base.',
             'id_usuario' => 1,
         ]);
 
-        $componente = Componente::where('nombre', 'Tarjeta Madre')->first();
-        Receta::create([
-            'id_joya' => $joya->id,
-            'id_componente' => $componente->id,
-            'cantidad' => 1,
+        \App\Models\Joya::create([
+            'nombre' => 'Pendiente Osito',
+            'descripcion' => 'Un delicado pendiente de disco duro y un osito en su centro.',
+            'id_usuario' => 3,
         ]);
 
-        $joya2 = Joya::create([
-            'nombre' => 'Pendiente de disco duro',
-            'descripcion' => 'Un pendiente único con un disco duro y un osito en su interior',
+        \App\Models\Joya::create([
+            'nombre' => 'Collar de RAM', 
+            'descripcion' => 'Un hermoso collar con detalles de una tarjeta RAM',
+            'id_usuario' => 3,
+        ]);
+
+        \App\Models\JOya::create([
+            'nombre' => 'Anillo de GPU',
+            'descripcion' => 'Delicado anillo con pequeños detalles de una GPU',
             'id_usuario' => 2,
         ]);
 
-        $componente2 = Componente::where('nombre', 'Almacenamiento')->first();
-        Receta::create([
-            'id_joya' => $joya2->id,
-            'id_componente' => $componente2->id,
-            'cantidad' => 1,
-        ]);
-
-        $componente3 = Componente::where('nombre', 'Pantalla de video')->first();
-        Receta::create([
-            'id_joya' => $joya2->id,
-            'id_componente' => $componente3->id,
-            'cantidad' => 1,
+        \App\Models\Joya::create([
+            'nombre' => 'Pulsera de SSD',
+            'descripcion' => 'Elegante pulsera de acero, cuero y detalles de una SSD',
+            'id_usuario' => 1,
         ]);
     }
 }

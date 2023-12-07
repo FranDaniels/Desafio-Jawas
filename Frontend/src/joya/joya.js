82d6bc790f48f04547515d76bdf62a175b609c6b
Marina Laguna <marinalaguna2004@gmail.com>
2023-12-07 15:46:30 +0100

    creacion del archivo joya.js

--- Frontend/src/joya/joya.js
+++ Frontend/src/joya/joya.js
@@ -18,7 +18,7 @@ async function mostrarJoyas() {
             return;
         }
 
-        joyas.forEach((joya) => {
+        function crearTarjeta(joya) {
             let tarjeta = document.createElement('div');
             tarjeta.classList.add('col-md-4', 'mb-4');
 
@@ -31,8 +31,46 @@ async function mostrarJoyas() {
                 </div>
             `;
 
-            contenedorJoyas.appendChild(tarjeta);
+            return tarjeta;
+        }
+
+        function crearItemLista(joya) {
+            let listItem = document.createElement('div');
+            listItem.classList.add('list-group-item', 'mb-2', 'border-bottom');
+
+            listItem.innerHTML = `
+                <h5 class="mb-1">${joya.nombre}</h5>
+                <p class="mb-1">${joya.descripcion}</p>
+            `;
+
+            return listItem;
+        }
+
+        joyas.forEach((joya) => {
+            contenedorJoyas.appendChild(crearTarjeta(joya));
         });
+
+        // Botón para cambiar entre vista de tarjetas y lista
+        const cambiarVistaBtn = document.getElementById('cambiarVista');
+
+        if (cambiarVistaBtn) {
+            cambiarVistaBtn.addEventListener('click', () => {
+                // Agrega o elimina la clase 'list-group' para cambiar la vista
+                contenedorJoyas.classList.toggle('list-group');
+
+                // Borra las tarjetas actuales
+                contenedorJoyas.innerHTML = '';
+
+                // Renderiza joyas en la nueva vista
+                joyas.forEach((joya) => {
+                    if (contenedorJoyas.classList.contains('list-group')) {
+                        contenedorJoyas.appendChild(crearItemLista(joya));
+                    } else {
+                        contenedorJoyas.appendChild(crearTarjeta(joya));
+                    }
+                });
+            });
+        }
 
     } catch (error) {
         console.error('Error al obtener las joyas:', error);

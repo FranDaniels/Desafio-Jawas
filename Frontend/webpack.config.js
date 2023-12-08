2417f1a3e3379a0ea2c2f7c7138f4693fe57766b
Marina Laguna <marinalaguna2004@gmail.com>
2023-12-08 20:17:40 +0100

    Configuración de webpack para que me añada los archivos css y sass

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -33,13 +33,6 @@ module.exports = {
           ],
         },
         {
-          test: /\.css$/,
-          use: [
-              "style-loader",
-              "css-loader"
-          ]
-        },
-        {
           test: /\.(png|jpe?g|gif)$/i,
           use: [
               {

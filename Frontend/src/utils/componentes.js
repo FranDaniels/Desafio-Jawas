ed2926dba8b5d4c04f3114ea77e2d4524c95aad7
Marina Laguna <marinalaguna2004@gmail.com>
2023-12-10 17:42:12 +0100

    Creada la funcion para cargar la barra de navegacion, añadido el icono para ir al perfil de usuario y enlazado el js donde se ha echo la funcion para usar la cabecera en mas archivos

---
+++ Frontend/src/utils/componentes.js
@@ -0,0 +1,13 @@
+
+function cabecera() {
+    document.getElementById("cabecera").innerHTML =
+        `
+        <header>
+            <a href="../inicio/inicio.html">Inicio</a>
+            <a href="../joya/joya.html"><p>Joyas</p></a>
+            <a href="../receta/receta.html"><p>Recetas</p></a>
+            <a href="../donar/donar.html"><p>Donar</p></a>
+            <a href="../perfil/perfil.html"><img src = "../imagenes/icono_perfil.png"></a>
+        </header>
+    `;
+}

cdd790c7f99d0ce13884815c10344a32a7f457ca
Marina Laguna <marinalaguna2004@gmail.com>
2023-12-11 13:53:31 +0100

    rutas colocadas dentro de la proteccion del middleware de auth para no poder acceder sin registrarse y sin realizar el login

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -21,61 +21,66 @@ use App\Http\Controllers\AuthController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::middleware('auth:sanctum')->group( function () {
-    //Añadir rutas para que no se puedan hacer si no se ha echo el registro ni el login
-});
-
-//Administrador
-//Hay que añadirle un middleware
-Route::get('admin/listarUsuarios',[controllerAdministrador::class,'listarUsuarios'])->middleware('admin');
-Route::get('admin/listarUsuario',[controllerAdministrador::class,'listarUsuario'])->middleware('admin');
-Route::get('admin/listarRoles/{id}',[controllerAdministrador::class,'listarRoles'])->middleware('admin');
-Route::get('admin/listarTodosLotes',[controllerAdministrador::class,'listarTodosLosLotes'])->middleware('admin');
-Route::post('admin/crearUsuario',[controllerAdministrador::class,'crearUsuario'])->middleware('admin');
-Route::post('admin/rol',[controllerAdministrador::class,'crearRol'])->middleware('admin');
-Route::post('admin/rolUsuario',[controllerAdministrador::class,'addRolUsuario'])->middleware('admin');
-Route::put('admin/modificarUsuario',[controllerAdministrador::class,'modificarUsuario'])->middleware('admin');
-Route::put('admin/modificarLote/{id}',[controllerAdministrador::class,'loteEntregado'])->middleware('admin');
-Route::put('admin/modificarPassword',[controllerAdministrador::class,'modificarPasswordUsuario'])->middleware('admin');
-Route::delete('admin/borrarUsuario',[controllerAdministrador::class,'borrarUsuario'])->middleware('admin');
-
-//Usuario
-Route::get('usuario/{id}',[controllerUsuario::class,'obtenerIdUsu']);
+   
 Route::post('registro',[controllerUsuario::class,'crearUsuario']);
-Route::put('modificarPass',[controllerUsuario::class,'modificarPassword']);
-Route::put('modificarDatos',[controllerUsuario::class,'modificarDatos']);
-
 Route::post('inicioSesion', [AuthController::class, 'inicioSesion']);
-
-Route::get('obtenerRol/{idUsuario}', [controllerRol::class, 'obtenerRolUsuario']);
-
-//Lotes
-Route::post('donar',[controllerUsuario::class,'donar']);
-
-//Clasificador
-Route::get('clasificador/componentes',[controllerClasificador::class,'listarComponentes']);
-Route::get('clasificador/listarLotesNombre',[controllerClasificador::class,'listarLoteNombreUsu']);
-Route::get('clasificador/listarLotes',[controllerClasificador::class,'listarLotes']);
-Route::get('clasificador/listarLote/{id}',[controllerClasificador::class,'listarLote']);
-Route::get('clasificador/listarMisLotes/{id}',[controllerClasificador::class,'listarMisLotes']);
-Route::get('clasificador/listarComponente/{id}',[controllerClasificador::class,'listarComponente']);
-Route::post('clasificador/despiece',[controllerClasificador::class,'realizarDespiece']);
-Route::post('clasificador/componente',[controllerClasificador::class,'crearComponente']);
-Route::post('clasificador/inventario',[controllerClasificador::class,'addInventario']);
-Route::post('clasificador/asignarLote',[controllerClasificador::class,'asignarLote']);
-
-//Recetas
-Route::get('mostrarRecetas', [controllerReceta::class, 'mostrarRecetas']);
-Route::get('obtenerNombreJoya/{id}', [ControllerJoyas::class, 'obtenerNombreJoya']);
-Route::get('obtenerNombreComponente/{id}',[controllerReceta::class,'obtenerNombreComponente']);
-Route::get('obtenerRecetaPorId/{id}', [controllerReceta::class, 'obtenerRecetaPorId']);
-
-//Joyas
-Route::get('mostrarJoyas', [ControllerJoyas::class, 'mostrarJoyas']);
 
 Route::get('', function () {
     return response()->json("No autorizado",203);
 })->name('nologin');
+
+//Añadir rutas para que no se puedan hacer si no se ha echo el registro ni el login
+Route::middleware('auth:sanctum')->group( function () {
+    
+    //Administrador
+    //Hay que añadirle un middleware
+    Route::get('admin/listarUsuarios',[controllerAdministrador::class,'listarUsuarios'])->middleware('admin');
+    Route::get('admin/listarUsuario',[controllerAdministrador::class,'listarUsuario'])->middleware('admin');
+    Route::get('admin/listarRoles/{id}',[controllerAdministrador::class,'listarRoles'])->middleware('admin');
+    Route::get('admin/listarTodosLotes',[controllerAdministrador::class,'listarTodosLosLotes'])->middleware('admin');
+    Route::post('admin/crearUsuario',[controllerAdministrador::class,'crearUsuario'])->middleware('admin');
+    Route::post('admin/rol',[controllerAdministrador::class,'crearRol'])->middleware('admin');
+    Route::post('admin/rolUsuario',[controllerAdministrador::class,'addRolUsuario'])->middleware('admin');
+    Route::put('admin/modificarUsuario',[controllerAdministrador::class,'modificarUsuario'])->middleware('admin');
+    Route::put('admin/modificarLote/{id}',[controllerAdministrador::class,'loteEntregado'])->middleware('admin');
+    Route::put('admin/modificarPassword',[controllerAdministrador::class,'modificarPasswordUsuario'])->middleware('admin');
+    Route::delete('admin/borrarUsuario',[controllerAdministrador::class,'borrarUsuario'])->middleware('admin');
+
+    //Usuario
+    Route::get('usuario/{id}',[controllerUsuario::class,'obtenerIdUsu']);
+    Route::put('modificarPass',[controllerUsuario::class,'modificarPassword']);
+    Route::put('modificarDatos',[controllerUsuario::class,'modificarDatos']);
+
+
+    //Lotes
+    Route::post('donar',[controllerUsuario::class,'donar']);
+
+    //Clasificador
+    Route::get('clasificador/componentes',[controllerClasificador::class,'listarComponentes']);
+    Route::get('clasificador/listarLotesNombre',[controllerClasificador::class,'listarLoteNombreUsu']);
+    Route::get('clasificador/listarLotes',[controllerClasificador::class,'listarLotes']);
+    Route::get('clasificador/listarLote/{id}',[controllerClasificador::class,'listarLote']);
+    Route::get('clasificador/listarMisLotes/{id}',[controllerClasificador::class,'listarMisLotes']);
+    Route::get('clasificador/listarComponente/{id}',[controllerClasificador::class,'listarComponente']);
+    Route::post('clasificador/despiece',[controllerClasificador::class,'realizarDespiece']);
+    Route::post('clasificador/componente',[controllerClasificador::class,'crearComponente']);
+    Route::post('clasificador/inventario',[controllerClasificador::class,'addInventario']);
+    Route::post('clasificador/asignarLote',[controllerClasificador::class,'asignarLote']);
+
+    //Recetas
+    Route::get('mostrarRecetas', [controllerReceta::class, 'mostrarRecetas']);
+    Route::get('obtenerNombreJoya/{id}', [ControllerJoyas::class, 'obtenerNombreJoya']);
+    Route::get('obtenerNombreComponente/{id}',[controllerReceta::class,'obtenerNombreComponente']);
+    Route::get('obtenerRecetaPorId/{id}', [controllerReceta::class, 'obtenerRecetaPorId']);
+
+    //Joyas
+    Route::get('mostrarJoyas', [ControllerJoyas::class, 'mostrarJoyas']);
+
+    //Rol
+    Route::get('obtenerRol/{idUsuario}', [controllerRol::class, 'obtenerRolUsuario']);
+});
+
+
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });

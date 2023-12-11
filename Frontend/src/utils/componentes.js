8bbe03424d76c96af30af376981a3ad2c5847bf9
Marina Laguna <marinalaguna2004@gmail.com>
2023-12-12 00:13:46 +0100

    Creación del pie de página

--- Frontend/src/utils/componentes.js
+++ Frontend/src/utils/componentes.js
@@ -30,3 +30,45 @@ export function cabecera() {
     </nav>
   `;
 }
+
+export function footer() {
+  document.getElementById("footer").innerHTML =
+      `
+<footer class="text-center bg-body-tertiary text-muted">
+  <section class="container text-center text-md-start mt-5">
+    <div class="row mt-3">
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+        <h6 class="text-uppercase fw-bold mb-4">
+          Productos
+        </h6>
+        <p>Collares</p>
+        <p>Pulseras</p>
+        <p>Pines</p>
+        <p>Pendientes</p>
+        <p>LLaveros</p>
+      </div>
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+        <h6 class="text-uppercase fw-bold mb-4">
+          Enlaces de la página
+        </h6>
+        <p><a class="nav-link" href="../inicio/inicio.html">Inicio</a></p>
+        <p><a class="nav-link" href="../joya/joya.html">Joyas</a></p>
+        <p><a class="nav-link" href="../receta/receta.html">Recetas</a></p>
+        <p><a class="nav-link" href="../donar/donar.html">Donar</a></p>
+      </div>
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+        <h6 class="text-uppercase fw-bold mb-4">Contacto</h6>
+        <p class="fas fa-home me-3">Paseo de San Gregorio, 82-84. 13500. Puertollano.</p>
+        <p class="fas fa-envelope me-3">info@example.com</p>
+        <p class="fas fa-phone me-3">+34 123 45 67 89</p>
+        <p class="fas fa-print me-3">+34 123 45 67 89</p>
+      </div>
+    </div>
+  </section>
+
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2023 Derechos de autor: Francisco Álvarez y Marina Laguna
+  </div>
+</footer>
+  `;
+}

ddfd2fdbb5190f1883e658957be21919bc387dfb
Marina Laguna <marinalaguna2004@gmail.com>
2023-12-12 10:29:10 +0100

    creadas las cabeceras para los distintos tipos de roles

--- Frontend/src/utils/componentes.js
+++ Frontend/src/utils/componentes.js
@@ -1,4 +1,4 @@
-export function cabecera() {
+export function cabeceraAdministrador() {
   document.getElementById("cabecera").innerHTML =
       `
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -18,7 +18,121 @@ export function cabecera() {
             <a class="nav-link" href="../receta/receta.html">Recetas</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="../donar/donar.html">Donar</a>
+            <a class="nav-link" href="../donar/donar.html">Donación</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../clasificacion/clasificacion.html">Clasificación</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../clasificar/clasificar.html">Lotes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../adminLote/adminLote.html">Administrar Lotes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../admin/admin.html">Administración</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../perfil/perfil.html">
+              <img src="../imagenes/icono_perfil.png" alt="Perfil" width="30" height="30">
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  `;
+}
+
+export function cabeceraColaborador() {
+  document.getElementById("cabecera").innerHTML =
+      `
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Joyería Jawas</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="../inicio/inicio.html">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../joya/joya.html">Joyas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../receta/receta.html">Recetas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../donar/donar.html">Donación</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../perfil/perfil.html">
+              <img src="../imagenes/icono_perfil.png" alt="Perfil" width="30" height="30">
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  `;
+}
+
+export function cabeceraDiseñador() {
+  document.getElementById("cabecera").innerHTML =
+      `
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Joyería Jawas</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="../inicio/inicio.html">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../joya/joya.html">Joyas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../receta/receta.html">Recetas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../perfil/perfil.html">
+              <img src="../imagenes/icono_perfil.png" alt="Perfil" width="30" height="30">
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  `;
+}
+
+export function cabeceraClasificador() {
+  document.getElementById("cabecera").innerHTML =
+      `
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Joyería Jawas</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="../inicio/inicio.html">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../joya/joya.html">Joyas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../receta/receta.html">Recetas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../clasificacion/clasificacion.html">Clasificación</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../receta/receta.html">Recetas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../clasificar/clasificar.html">Lotes</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="../perfil/perfil.html">
